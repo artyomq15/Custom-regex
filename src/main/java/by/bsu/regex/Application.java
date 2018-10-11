c5dd0bfe27c88d161db94ae1f7184d887fe12199
Artsiom Stankevich <Artsiom_Stankevich@epam.com>
2018-10-11 20:26:38 +0300

    add normalization by star

--- src/main/java/by/bsu/regex/Application.java
+++ src/main/java/by/bsu/regex/Application.java
@@ -1,7 +1,11 @@
 package by.bsu.regex;
 
+import by.bsu.regex.core.CustomRegex;
+
 public class Application {
     public static void main(String[] args) {
+        CustomRegex customRegex = new CustomRegex("mc*(m|t)*m(c)*");
+        customRegex.match("input");
 
     }
 }
--- src/main/java/by/bsu/regex/core/CustomRegex.java
+++ src/main/java/by/bsu/regex/core/CustomRegex.java
@@ -1,5 +1,9 @@
 package by.bsu.regex.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CustomRegex {
 
     private String expression;
@@ -14,4 +18,44 @@ public class CustomRegex {
         this.expression = expression;
     }
 
+    public boolean match(String input) {
+
+        List<Character> characterList = new ArrayList<>();
+
+        for (char c : expression.toCharArray()) {
+            characterList.add(c);
+        }
+
+        System.out.println(characterList);
+        System.out.println(normalizeRegexByStars(characterList));
+
+        return false;
+    }
+
+    private List<Character> normalizeRegexByStars(List<Character> regex) {
+        List<Character> normalized = new ArrayList<>();
+
+        for (int i = 0; i < regex.size(); i++) {
+            normalized.add(regex.get(i));
+
+            if (regex.get(i) == STAR) {
+                normalized.add(CLOSE);
+
+                for (int j = normalized.size() - 3, counter = 0; j >= 0; j--) {
+                    if (normalized.get(j) == CLOSE) {
+                        counter++;
+                    } else if (normalized.get(j) == OPEN) {
+                        counter--;
+                    }
+
+                    if (counter == 0) {
+                        normalized.add(j, OPEN);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return normalized;
+    }
 }
